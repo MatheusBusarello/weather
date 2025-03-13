c3fe28d4f86b48bfaf0615f7982e9807a394821e
MatheusBusarello <matheus.b.almeida@outlook.com>
2025-03-13 06:40:25 -0300

    correção logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 export function Logo() {
   return (
     <div className='logo'>
-      <img src="../../assets/img/logo.png" alt="Logo" />
+      <img src="/logo.png" alt="Logo" />
     </div>
   )
 }
